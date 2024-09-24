8ec0b6c53ae3d972aae766a947284c15748cb4e6
JohnnyLomas <johnnylo245@gmail.com>
2024-09-24 09:30:33 -0700

    Retrain generation

--- finetune_AgroSegmentNT.py
+++ finetune_AgroSegmentNT.py
@@ -4,15 +4,16 @@ from torch.utils.data.distributed import DistributedSampler
 import torch.optim as optim
 from torch.nn.utils import clip_grad_norm_
 from transformers import get_linear_schedule_with_warmup
+from safetensors.torch import save_model
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from accelerate import Accelerator
 from accelerate import DistributedDataParallelKwargs
 from utils_transgenic import *
 
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+#os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 os.environ['HF_HOME'] = './HFmodels'
-os.environ["NCCL_DEBUG"] = "INFO"
+#os.environ["NCCL_DEBUG"] = "INFO"
 
 def trainSegmentNTAccelerate(
 	train_ds:isoformData, 
@@ -55,19 +56,20 @@ def trainSegmentNTAccelerate(
 	print(f"Training SegmentNT. {checkpoint_path=} {output_dir=} {safetensors_model=}", file=sys.stderr)
 	
 	# Set up accelerator
-	ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-	accelerator = Accelerator(kwargs_handlers=[ddp_kwargs]) # gradient_accumulation_steps=32
-	device = accelerator.device
+	#ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+	#accelerator = Accelerator(kwargs_handlers=[ddp_kwargs]) # gradient_accumulation_steps=32
+	#device = accelerator.device
+	device = torch.device("cuda")
 	print(f"Training SegmentNT with Accelerate on {device}", file=sys.stderr)
 	
 	# Set up DataLoaders
-	torch.manual_seed(234)
-	torch.cuda.manual_seed_all(234)
+	torch.manual_seed(123)
+	torch.cuda.manual_seed_all(123)
 	train_ds = makeDataLoader(train_ds, shuffle=True, batch_size=batch_size, pin_memory=True, num_workers=4, collate_fn=segment_collate_fn)
 	eval_ds = makeDataLoader(eval_ds, shuffle=True, batch_size=batch_size, pin_memory=True, num_workers=4, collate_fn=segment_collate_fn)
 	
 	# initialize the model
-	model = segmented_sequence_embeddings(encoder_model, segmentation_model, 14)
+	model = segmented_sequence_embeddings(encoder_model, segmentation_model, 14, do_segment=True)
 	model.train()
 	for param in model.esm.parameters():
 		param.requires_grad = False
@@ -75,8 +77,8 @@ def trainSegmentNTAccelerate(
 		param.requires_grad = False
 	for param in model.hidden_mapping_layernorm.parameters():
 		param.requires_grad = False
-	for param in model.film.parameters():
-		param.requires_grad = False
+	#for param in model.film.parameters():
+	#	param.requires_grad = False
 	
 	if safetensors_model:
 		tensors = {}
@@ -100,18 +102,19 @@ def trainSegmentNTAccelerate(
 		)
 	
 	# Prep objects for use with accelerator
-	model, optimizer, train_ds, eval_ds, lr_scheduler = accelerator.prepare(
-		model, optimizer, train_ds, eval_ds, lr_scheduler
-	)
+	#model, optimizer, train_ds, eval_ds, lr_scheduler = accelerator.prepare(
+	#	model, optimizer, train_ds, eval_ds, lr_scheduler
+	#)
 	
 	# Training loop
 	best_eval_score = None
 	for epoch in range(num_epochs):
 		total_loss = 0
 		for step, batch in enumerate(tqdm(train_ds, miniters=10, disable=False)):
-			outputs = model(batch[0], attention_mask=batch[1], segLabels=batch[2])
+			outputs = model(batch[0].to(device), attention_mask=batch[1].to(device), segLabels=batch[2].to(device))
 			loss = outputs[3] / accumulation_steps
-			accelerator.backward(loss)
+			#accelerator.backward(loss)
+			loss.backward()
 			total_loss += outputs[3].detach().float()
 			if (step+1) % accumulation_steps == 0:
 				clip_grad_norm_(model.parameters(), max_grad_norm)
@@ -126,9 +129,11 @@ def trainSegmentNTAccelerate(
 				wandb.log(wandb_log)
 				optimizer.zero_grad()
 			
-			if (step % 5000 == 0):
+			if (step % 300 == 0):
 				print(f"Epoch {epoch=}, Step {step=}, Loss {loss=}", file=sys.stderr)
-				accelerator.save_state(output_dir=checkpoint_path)
+				#accelerator.save_state(output_dir=checkpoint_path)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
+				
 
 		train_epoch_loss = total_loss / len(train_ds)
 		train_ppl = torch.exp(train_epoch_loss)
@@ -155,14 +160,16 @@ def trainSegmentNTAccelerate(
 				best_eval_score = eval_ppl
 				if not os.path.exists("checkpoints"):
 					os.makedirs("checkpoints", exist_ok=True)
-				accelerator.save_state(output_dir=checkpoint_path)
+				#accelerator.save_state(output_dir=checkpoint_path)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
 				print(f"New best model saved with {eval_epoch_loss=}", file=sys.stderr)
 		else:
 			if best_eval_score is None or train_epoch_loss < best_eval_score:
 				best_eval_score = train_ppl
 				if not os.path.exists("checkpoints"):
 					os.makedirs("checkpoints", exist_ok=True)
-				accelerator.save_state(output_dir=checkpoint_path)
+				#accelerator.save_state(output_dir=checkpoint_path)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
 				print(f"New best model saved with {train_epoch_loss=}", file=sys.stderr)
 		
 		torch.cuda.empty_cache()
@@ -173,8 +180,8 @@ def trainSegmentNTAccelerate(
 		train_ppl = 0
 
 	
-	accelerator.wait_for_everyone()
-	accelerator.save_model(model, output_dir)
+	#accelerator.wait_for_everyone()
+	#accelerator.save_model(model, output_dir)
 	wandb.finish()
 
 
@@ -184,23 +191,23 @@ if __name__ == '__main__':
 	encoder_model = "InstaDeepAI/agro-nucleotide-transformer-1b"
 	segmentation_model = "InstaDeepAI/segment_nt_multi_species"
 
-	ds  = segmentationDataset(12288, 11000, "Segmentation_7Genomes.db")
-	train_data, eval_data, test_data = torch.utils.data.random_split(ds, [231003, 30800, 46202])
+	ds  = segmentationDataset(6144, 6000, "/home/jlomas/Segmentation_10Genomes.db")
+	train_data, eval_data, test_data = torch.utils.data.random_split(ds, [800419, 106722,160085])
 
 	trainSegmentNTAccelerate(
 		train_data, 
 		eval_data, 
-		lr=1e-5, 
+		lr=1e-4, 
 		num_epochs=5, 
 		schedule_lr=True, 
 		eval=True, 
-		batch_size=1, 
-		accumulation_steps=64,
-		checkpoint_path="checkpoints_SegmentNT/", 
-		safetensors_model="checkpoints_SegmentNT/model.safetensors",
+		batch_size=16, 
+		accumulation_steps=16,
+		checkpoint_path="checkpoints_SegmentNT", 
+		safetensors_model=None, #"checkpoints_SegmentNT/model.safetensors",
 		output_dir="saved_models_SegmentNT/",
 		max_grad_norm=2,
-		notes="Direct fine tuning of agro SegmentNT",
+		notes="Short length, direct fine tuning of agro SegmentNT",
 		encoder_model=encoder_model,
 		unlink = False
 	)
--- train_transgenic.py
+++ train_transgenic.py
@@ -9,10 +9,11 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from accelerate import Accelerator
 from accelerate import DistributedDataParallelKwargs
 from utils_transgenic import *
+from safetensors.torch import save_model
 
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+#os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 os.environ['HF_HOME'] = './HFmodels'
-os.environ["NCCL_DEBUG"] = "INFO"
+#os.environ["NCCL_DEBUG"] = "INFO"
 
 def trainTransgenicFCGAccelerate(
 	train_ds:isoformData, 
@@ -30,6 +31,11 @@ def trainTransgenicFCGAccelerate(
 	notes="",
 	encoder_model="InstaDeepAI/nucleotide-transformer-v2-500m-multi-species",
 	unlink=False):
+
+	if not os.path.exists(checkpoint_path):
+		os.makedirs(checkpoint_path)
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
 
 	# start a new wandb run to track this script
 	wandb.init(
@@ -55,9 +61,10 @@ def trainTransgenicFCGAccelerate(
 	print(f"Training transgenic with reinitialized decoder. {checkpoint_path=} {output_dir=} {safetensors_model=}", file=sys.stderr)
 	
 	# Set up accelerator
-	ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-	accelerator = Accelerator(kwargs_handlers=[ddp_kwargs]) # gradient_accumulation_steps=32
-	device = accelerator.device
+	#ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+	#accelerator = Accelerator(kwargs_handlers=[ddp_kwargs]) # gradient_accumulation_steps=32
+	#device = accelerator.device
+	device = torch.device("cuda")
 	print(f"Training transgenic with Accelerate on {device}", file=sys.stderr)
 	
 	# Set up DataLoaders
@@ -66,7 +73,7 @@ def trainTransgenicFCGAccelerate(
 	train_ds = makeDataLoader(train_ds, shuffle=True, batch_size=batch_size, pin_memory=True, num_workers=4)
 	eval_ds = makeDataLoader(eval_ds, shuffle=True, batch_size=batch_size, pin_memory=True, num_workers=4)
 	
-	model = getPeftModel(encoder_model, config=None, unlink=False, safetensors_model=safetensors_model, device=device, mode="train")
+	model = getModel(None, safetensors_model=safetensors_model, device="cpu", mode="train")
 	model.to(device)
 	model.train()
 	
@@ -83,35 +90,39 @@ def trainTransgenicFCGAccelerate(
 		)
 	
 	# Prep objects for use with accelerator
-	model, optimizer, train_ds, eval_ds, lr_scheduler = accelerator.prepare(
-		model, optimizer, train_ds, eval_ds, lr_scheduler
-	)
+	#model, optimizer, train_ds, eval_ds, lr_scheduler = accelerator.prepare(
+	#	model, optimizer, train_ds, eval_ds, lr_scheduler
+	#)
 	
 	# Training loop
 	best_eval_score = None
 	for epoch in range(num_epochs):
 		total_loss = 0
 		for step, batch in enumerate(tqdm(train_ds, miniters=10, disable=False)):
-			outputs = model(input_ids=batch[0], attention_mask=batch[1], global_attention_mask=batch[2], labels=batch[3], return_dict=True)
-			loss = outputs.loss / accumulation_steps
-			accelerator.backward(loss)
+			ii, am, gam, lab = batch[0].to(device), batch[1].to(device), batch[2], batch[3].to(device)
+			outputs = model(input_ids=ii, attention_mask=am, global_attention_mask=gam, labels=lab, return_dict=True)
 			total_loss += outputs.loss.detach().float()
+			outputs.loss = outputs.loss / accumulation_steps
+			outputs.loss.backward()
+			#accelerator.backward(outputs.loss / accumulation_steps)
 			if (step+1) % accumulation_steps == 0:
 				clip_grad_norm_(model.parameters(), max_grad_norm)
 				optimizer.step()
 				if schedule_lr: lr_scheduler.step()
 				# log metrics to wandb
-				wandb_log = {"epoch":epoch, "step":step, "loss": outputs.loss.detach().float(), "mean_loss": total_loss / (step+1), "lr": lr_scheduler.get_last_lr()[0]}
+				wandb_log = {"epoch":epoch, "step":step, "loss": outputs.loss.detach().float()*accumulation_steps, "mean_loss": total_loss / (step+1), "lr": lr_scheduler.get_last_lr()[0]}
 				for name, param in model.named_parameters():
 					if (param.grad != None) & (param.requires_grad):
-						grad_norm = param.grad.norm().item()
+						grad_norm = param.grad.norm().detach().item()
 						wandb_log[f"{name}_grad_norm"] = grad_norm
 				wandb.log(wandb_log)
 				optimizer.zero_grad()
 			
-			if (step % 5000 == 0) & (step != 0):
-				print(f"Epoch {epoch=}, Step {step=}, Loss {loss=}", file=sys.stderr)
-				accelerator.save_state(output_dir=checkpoint_path)
+			if (step % 1000 == 0) & (step != 0):
+				print(f"Epoch {epoch}, Step {step}, Loss {outputs.loss.detach().float()*accumulation_steps}", file=sys.stderr)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
+			del outputs
+			torch.cuda.empty_cache()
 
 		train_epoch_loss = total_loss / len(train_ds)
 		train_ppl = torch.exp(train_epoch_loss)
@@ -121,9 +132,8 @@ def trainTransgenicFCGAccelerate(
 			eval_loss = 0
 			for batch in tqdm(eval_ds, miniters=10, disable=False):
 				with torch.no_grad():
-					outputs = model(input_ids=batch[0], attention_mask=batch[1], global_attention_mask=batch[2], labels=batch[3], return_dict=True)
-				loss = outputs.loss
-				eval_loss += loss.detach().float()
+					outputs = model(input_ids=batch[0].to(device), attention_mask=batch[1].to(device), global_attention_mask=batch[2], labels=batch[3].to(device), return_dict=True)
+				eval_loss += outputs.loss.detach().float()
 
 			eval_epoch_loss = eval_loss / len(eval_ds)
 			eval_ppl = torch.exp(eval_epoch_loss)
@@ -138,26 +148,23 @@ def trainTransgenicFCGAccelerate(
 				best_eval_score = eval_ppl
 				if not os.path.exists("checkpoints"):
 					os.makedirs("checkpoints", exist_ok=True)
-				accelerator.save_state(output_dir=checkpoint_path)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
 				print(f"New best model saved with {eval_epoch_loss=}", file=sys.stderr)
 		else:
 			if best_eval_score is None or train_epoch_loss < best_eval_score:
 				best_eval_score = train_ppl
 				if not os.path.exists("checkpoints"):
 					os.makedirs("checkpoints", exist_ok=True)
-				accelerator.save_state(output_dir=checkpoint_path)
+				save_model(model, f"{checkpoint_path}/model.safetensors")
 				print(f"New best model saved with {train_epoch_loss=}", file=sys.stderr)
 		
 		torch.cuda.empty_cache()
 		gc.collect()
 		total_loss = 0
-		loss = None
+		#loss = None
 		train_epoch_loss = 0
 		train_ppl = 0
 
-	
-	accelerator.wait_for_everyone()
-	accelerator.save_model(model, output_dir)
 	wandb.finish()
 
 
@@ -166,33 +173,38 @@ if __name__ == '__main__':
 
 	#InstaDeepAI/nucleotide-transformer-v2-500m-multi-species
 	#InstaDeepAI/agro-nucleotide-transformer-1b
-	encoder_model = sys.argv[1]
-	unlink = bool(sys.argv[2])
-	notes = sys.argv[3]
+	#encoder_model = sys.argv[1]
+	#unlink = bool(sys.argv[2])
+	#notes = sys.argv[3]
 
-	with open("Train-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
-		train_data = pickle.load(f)
-	with open("Eval-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
-		eval_data = pickle.load(f)
-	with open("Test-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
-		test_data = pickle.load(f)
+	#with open("Train-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
+	#	train_data = pickle.load(f)
+	#with open("Eval-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
+	#	eval_data = pickle.load(f)
+	#with open("Test-Flagship_Genomes_49k_extra200_addRCIsoOnly.pkl", 'rb') as f:
+	#	test_data = pickle.load(f)
+
+	db="Generation_10G_static6144_addExtra200_addRCIsoOnly_clean.db"
+	dt = GFFTokenizer()
+	ds = isoformData(db, dt, mode="training", encoder_model="InstaDeepAI/agro-nucleotide-transformer-1b", global_attention=False)
+	train_data, eval_data, test_data = torch.utils.data.random_split(ds, [339817, 45309,67964])
 
 	trainTransgenicFCGAccelerate(
 		train_data, 
 		eval_data, 
 		lr=1e-4, 
-		num_epochs=4, 
+		num_epochs=10, 
 		schedule_lr=True, 
 		eval=True, 
 		batch_size=1, 
 		accumulation_steps=16,
-		checkpoint_path="checkpoints_ESMpeftReal_local09/", 
-		safetensors_model="checkpoints_ESMpeftReal_local09/model.safetensors", #"saved_models_FCG/model.safetensors",
-		output_dir="saved_models_ESMpeftReal_local09/",
+		checkpoint_path="checkpoints_Gen10G/", 
+		safetensors_model="checkpoints_Gen10G/model.safetensors",
+		output_dir="saved_models_Gen10G/",
 		max_grad_norm=1,
-		notes=notes,
-		encoder_model=encoder_model,
-		unlink = unlink
+		notes="Continuing training with 3 more genomes and no PEFT",
+		encoder_model="InstaDeepAI/agro-nucleotide-transformer-1b",
+		unlink = False
 	)
 
 #	Keeping the training, validation, and testing datasets consistent after updating with reverse complemented isoforms
